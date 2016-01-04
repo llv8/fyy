e900a1b90b48545977bd3629017db97af8d515ef
lewis <venlv2046@gmail.com>
2016-01-04 23:51:44 +0800

    init

--- src/main/java/com/fy/fyy/back/bean/Material.java
+++ src/main/java/com/fy/fyy/back/bean/Material.java
@@ -1,5 +1,119 @@
 package com.fy.fyy.back.bean;
 
 public class Material {
+	
+	private Long id;
+	private String name;
+	private Category category;
+	private Unit unit;
+	private Integer num;
+	private String note;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+
+	public Integer getNum() {
+		return num;
+	}
+
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public static enum Category {
+		outer_package(1, "外包装"), inner_package(2, "内包装"), desiccant(3, "干燥剂");
+
+		private int id;
+		private String name;
+
+		private Category(int id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
+	
+	public static enum Unit {
+		unit_per(1, "个"), unit_kg(2, "公斤"), unit_package(3, "箱");
+
+		private int id;
+		private String name;
+
+		private Unit(int id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
 }
--- src/main/java/com/fy/fyy/back/bean/User.java
+++ src/main/java/com/fy/fyy/back/bean/User.java
@@ -13,33 +13,57 @@ public class User {
 	private String password;
 	private Date createDate;
 	private Status status;
+
+	public static User getInstance(Long id) {
+		User usr = new User();
+		usr.setId(id);
+		return usr;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getLoginName() {
 		return loginName;
 	}
+
 	public void setLoginName(String loginName) {
 		this.loginName = loginName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
 	public Status getStatus() {
 		return status;
 	}
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
